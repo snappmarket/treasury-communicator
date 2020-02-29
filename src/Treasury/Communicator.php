d83fccd1ec6c368d33585068a259eb0327ae8284
EmiTis <emitis89@gmail.com>
2020-02-29 20:28:08 +0330

    provide the specific communicator

---
+++ src/Treasury/Communicator.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace SnappMarket\Treasury;
+
+use SnappMarket\Communicator\Communicator as BasicCommuncator;
+
+
+class Communicator extends BasicCommuncator
+{
+    // todo: to be filled with specific methods for the treasury service
+}
