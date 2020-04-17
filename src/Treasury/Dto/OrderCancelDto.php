4ae07146e64c5b6addbd80f56a1def835a4aadcb
Pooria Karimi <pooria.karimi@zoodfood.com>
2020-04-17 23:23:23 +0430

    Added cancel order request.

---
+++ src/Treasury/Dto/OrderCancelDto.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace SnappMarket\Treasury\Dto;
+
+
+class OrderCancelDto
+{
+    /** @var int */
+    private $orderId;
+
+    /** @var int */
+    private $creatorId;
+
+    public function setOrderId(int $value)
+    {
+        $this->orderId = $value;
+
+        return $this;
+    }
+
+
+
+    public function getOrderId()
+    {
+        return $this->orderId;
+    }
+
+
+
+    public function setCreatorId(int $value)
+    {
+        $this->creatorId = $value;
+
+        return $this;
+    }
+
+
+
+    public function getCreatorId()
+    {
+        return $this->creatorId;
+    }
+
+}
